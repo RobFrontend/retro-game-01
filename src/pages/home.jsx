b8b05b236fdcfdbf783853bba7da7e3b33898488
RobFrontend <robert.grabowski97@gmail.com>
2024-10-07 23:00:13 +0200

    final 1.0

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,17 +12,25 @@ export default function About() {
   const [isPointTop, setIsPointTop] = useState(
     Math.floor(Math.random() * 1000)
   );
+  const [isDangerLeft, setIsDangerLeft] = useState(null);
+  const [isDangerTop, setIsDangerTop] = useState(null);
+  const [isDanger2Left, setIsDanger2Left] = useState(null);
+  const [isDanger2Top, setIsDanger2Top] = useState(null);
+  const [isDanger3Left, setIsDanger3Left] = useState(null);
+  const [isDanger3Top, setIsDanger3Top] = useState(null);
   const [isTimeEnd, setIsTimeEnd] = useState(false);
   const [isScore, setIsScore] = useState(0);
   const [isRestart, setIsRestart] = useState(false);
   const [isHighScore, setIsHighScore] = useState(0);
   const [isCar, setIsCar] = useState("bluecar.png");
+  const [isInfo, setIsInfo] = useState(true);
 
   // *****************
   // Starting game by clicking on vehicle/hero
   function hideStart() {
     setIsStart(true);
     setIsRestart(false);
+    setIsInfo(false);
   }
 
   // *******************
@@ -34,6 +42,25 @@ export default function About() {
     }
   }, [toTop, toSide, isRestart]);
 
+  // Danger points
+  useEffect(() => {
+    if (isScore > 2 && !isRestart) {
+      setIsDangerLeft(Math.floor(Math.random() * 1560));
+      setIsDangerTop(Math.floor(Math.random() * 940));
+    }
+    if (isScore > 5 && !isRestart) {
+      const danger2Position = setInterval(() => {
+        setIsDanger2Left(Math.floor(Math.random() * 1560));
+        setIsDanger2Top(Math.floor(Math.random() * 940));
+        if (isScore > 8 && !isRestart) {
+          setIsDanger3Left(Math.floor(Math.random() * 1560));
+          setIsDanger3Top(Math.floor(Math.random() * 940));
+        }
+      }, 2000);
+      return () => clearInterval(danger2Position);
+    }
+  }, [isScore]);
+
   // *************************************
   // Restarting, Starting, Time ending, Counting, Point icon
   useEffect(() => {
@@ -43,6 +70,12 @@ export default function About() {
       // setIsPointTop(Math.floor(Math.random() * 940));
       setIsPointLeft(null);
       setIsPointTop(null);
+      setIsDangerLeft(null);
+      setIsDangerTop(null);
+      setIsDanger2Left(null);
+      setIsDanger2Top(null);
+      setIsDanger3Left(null);
+      setIsDanger3Top(null);
       setIsTime(15);
       setIsScore(0);
       setIsTimeEnd(false);
@@ -56,6 +89,13 @@ export default function About() {
         setIsPointTop(Math.floor(Math.random() * 940));
         setIsRestart(false);
       }
+      // Danger point
+      // if (isScore > 0 && !isRestart) {
+      //   setIsDangerLeft(Math.floor(Math.random() * 1560));
+      //   setIsDangerTop(Math.floor(Math.random() * 940));
+      //   console.log(setIsDangerLeft);
+      // }
+
       // Counting
       if (isStart && isTime > 0) {
         const timeLeft = setInterval(() => {
@@ -69,6 +109,12 @@ export default function About() {
         setIsPointTop(null);
         setIsTimeEnd(true);
         setIsStart(false);
+        setIsDangerLeft(null);
+        setIsDangerTop(null);
+        setIsDanger2Left(null);
+        setIsDanger2Top(null);
+        setIsDanger3Left(null);
+        setIsDanger3Top(null);
       }
     }
   }, [isStart, isTime, isPointLeft, isPointTop, isRestart, isScore, isTimeEnd]);
@@ -82,8 +128,7 @@ export default function About() {
     if (isCar === "redcar.png") carSpeed = 9;
     if (isCar === "blackcar.png") carSpeed = 11;
     e.preventDefault();
-    // e.stopPropagation();
-    console.log(e.key);
+
     if (isStart && !isTimeEnd) {
       // *********************************
       // steering
@@ -118,12 +163,6 @@ export default function About() {
       if (e.key === "Escape") {
         setIsRestart(true);
       }
-      // if (
-      //   (toSide === isPointLeft - 20 || toSide === isPointLeft + 20) &&
-      //   (toTop === isPointTop - 20 || toTop === isPointTop + 20)
-      // ) {
-      //   setIsTime((t) => t + 20);
-      // }
       // ***************************
       // Gaining points
       if (
@@ -132,10 +171,24 @@ export default function About() {
         toTop >= isPointTop - 40 &&
         toTop <= isPointTop + 40
       ) {
-        setIsTime((t) => t + 10);
+        if (isScore <= 2) setIsTime((t) => t + 8);
+        if (isScore > 2 && isScore <= 7) setIsTime((t) => t + 6);
+        if (isScore > 7 && isScore <= 9) setIsTime((t) => t + 4);
+        if (isScore > 9) setIsTime((t) => t + 2);
         setIsPointLeft(null);
         setIsPointTop(null);
         setIsScore((s) => s + 1);
+      }
+      // Minus points for danger
+      if (
+        toSide >= isDangerLeft - 40 &&
+        toSide <= isDangerLeft + 40 &&
+        toTop >= isDangerTop - 40 &&
+        toTop <= isDangerTop + 40
+      ) {
+        setIsTime((t) => t - 10);
+        setIsDangerLeft(null);
+        setIsDangerTop(null);
       }
     }
   };
@@ -145,19 +198,6 @@ export default function About() {
 
   return (
     <div className="grid min-h-screen bg-slate-800">
-      {/* Timer */}
-
-      <div className="absolute text-slate-700 top-12 right-1/2 translate-x-1/2 z-20 flex gap-6 items-end">
-        <h2 className="text-xl">
-          Score: <span className="font-bold">{isScore}</span>
-        </h2>
-        <h2 className="text-4xl">
-          Time left: <span className="font-bold">{isTime}</span>
-        </h2>
-        <h2 className="text-xl">
-          HighScore: <span className="font-bold">{isHighScore}</span>
-        </h2>
-      </div>
       {/* Legend */}
       <div className="absolute top-12 right-8 text-slate-100">
         <button
@@ -166,6 +206,10 @@ export default function About() {
         >
           Restart
         </button>
+        <h2 className="mb-8">
+          or <b>Escape</b>
+          <br></br> to go Restart
+        </h2>
         <h2>
           <b>W</b> to go Up
         </h2>
@@ -177,6 +221,12 @@ export default function About() {
         </h2>
         <h2>
           <b>D</b> to go Right
+        </h2>
+        <h2>
+          <b>E</b> Top Right
+        </h2>
+        <h2>
+          <b>Q</b> Top Left
         </h2>
       </div>
       {/* Choose Car */}
@@ -244,11 +294,25 @@ export default function About() {
           </div>
         </div>
       )}
-      {/* Start Info */}
+
       <div
         className="h-[1000px] w-[1600px] relative bg-transparent   justify-self-center self-center"
         style={{ backgroundColor: "#E2E8F0" }}
       >
+        {/* Timer and score*/}
+
+        <div className="absolute text-slate-700 top-4 right-1/2 translate-x-1/2 z-20 flex gap-6 items-end">
+          <h2 className="text-xl">
+            Score: <span className="font-bold">{isScore}</span>
+          </h2>
+          <h2 className="text-4xl">
+            Time left: <span className="font-bold">{isTime}</span>
+          </h2>
+          <h2 className="text-xl">
+            HighScore: <span className="font-bold">{isHighScore}</span>
+          </h2>
+        </div>
+        {/* Start Info */}
         {isStart && !isRestart ? null : (
           <p
             className="z-20 absolute bg-green-700 text-green-50 p-2 rounded-full opacity-90"
@@ -260,6 +324,24 @@ export default function About() {
             Click on car to Start!
           </p>
         )}
+
+        {isInfo ? (
+          <div>
+            <h2 className="text-center p-4 text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-slate-900 text-slate-200">
+              1* If your screen resolution is{" "}
+              <span className="font-bold">less then 1920x1080</span> and you
+              dont have full screen mode option just go with{" "}
+              <span className="font-bold">'ctrl' + '-'</span> to fit the game
+              screen to your resolution.<br></br>
+              You should be able to see game map, restart button with legend on
+              the right and Car list on the left.<br></br>
+              2* If you want to change car after setting correct highscore click
+              'Escape' or 'Restart'<br></br>
+              Dont refresh the page to change your car<br></br>
+              3* Click on car to start! Have Fun!
+            </h2>
+          </div>
+        ) : null}
         {/* Car */}
         <button onKeyDown={key} onClick={hideStart} className="bg-none">
           <img
@@ -284,15 +366,40 @@ export default function About() {
             }}
           />
         ) : null}
-        {/* <div
-          className="z-40 absolute"
-          style={{
-            bottom: `${isPointTop}px`,
-            left: `${isPointLeft}px`,
-          }}
-        >
-          💙
-        </div> */}
+
+        {/* Danger */}
+        {isDangerLeft ? (
+          <img
+            src="danger.png"
+            className="z-40 absolute max-w-8 drop-shadow-md"
+            style={{
+              bottom: `${isDangerTop}px`,
+              left: `${isDangerLeft}px`,
+            }}
+          />
+        ) : null}
+        {/* Danger2 */}
+        {isDanger2Left ? (
+          <img
+            src="danger.png"
+            className="z-40 absolute max-w-8 drop-shadow-md"
+            style={{
+              bottom: `${isDanger2Top}px`,
+              left: `${isDanger2Left}px`,
+            }}
+          />
+        ) : null}
+        {/* Danger3 */}
+        {isDanger3Left ? (
+          <img
+            src="danger.png"
+            className="z-40 absolute max-w-8 drop-shadow-md"
+            style={{
+              bottom: `${isDanger3Top}px`,
+              left: `${isDanger3Left}px`,
+            }}
+          />
+        ) : null}
       </div>
       {/* Game Over */}
       {isTimeEnd && !isRestart ? (
